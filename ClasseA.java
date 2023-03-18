5deebb6c4606a24572a06744c232762f981dd2b3
Elias <elias.miranda@estudante.ufla.br>
2023-03-18 16:54:19 -0300

    Atendoe o enuciado 2

---
+++ ClasseA.java
@@ -0,0 +1,38 @@
+/*============================================================
+File: ClasseA.java
+Data: 25-11-22_22
+-------------------------------------------------------------
+Graduando: ELIAS DE JESUS MIRANDA 
+ANTONIO MARIA PEREIRA DE RESENDE - DOCENTE - DCC 
+Disciplina:Engenharia de Software 
+TURMA: GCC188-2022/2-V1-10A-G010 
+-------------------------------------------------------------*/
+
+class ClasseA {
+    private int a1;
+    private int a2;
+
+    public ClasseA(){
+        a1 = 0;        
+        a2 = 0;
+    }
+    public void getA1(){
+        System.out.print("getA1()");
+    }
+    public void getA2(){
+        System.out.print("getA2()");
+    }
+    public void setA1(){
+        System.out.print("setA1()");
+    }
+    public void setA2(){
+        System.out.print("setA2()");
+    }
+    public void mA1(){
+        System.out.print("mA1()");
+    }
+    public void mA2(){
+        System.out.print("mA2()");
+    }
+
+}
---
+++ ClasseB.java
@@ -0,0 +1,85 @@
+/*============================================================
+File: ClasseA.java
+Data: 25-11-22_22
+-------------------------------------------------------------
+Graduando: ELIAS DE JESUS MIRANDA 
+ANTONIO MARIA PEREIRA DE RESENDE - DOCENTE - DCC 
+Disciplina:Engenharia de Software 
+TURMA: GCC188-2022/2-V1-10A-G010 
+-------------------------------------------------------------*/
+
+class ClasseB{
+    private int b1;
+    private int b2;
+
+    public ClasseB(){
+        b1 = 0;        
+        b2 = 0;
+    }
+    public void getB1(){
+        System.out.print("getB1()");
+    }
+    public void getB2(){
+        System.out.print("getB2()");
+    }
+    public void setB1(){
+        System.out.print("setB1()");
+    }
+    public void setB2(){
+        System.out.print("setB2()");
+    }
+    public void mB1(){
+        System.out.print("mB1()");
+    }
+    public void mB2(){
+        System.out.print("mB2()");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class ClasseB{
+    private int b1;
+    private float b2;
+
+    public ClasseB(){
+        b1= 0;     
+        b2= 0;    
+    }
+
+    public void getB1(){
+       return beUm; 
+    }
+
+    public String getMatricula(){
+       return beDois; 
+    }
+
+    public void setNome(String novoBeUm){
+        this.beUm = novoBeUm;
+    }
+
+    public void setMatricula(String novoBeDois){
+        this.beDois = novoBeDois;
+    }
+    
+    public void metodoBTres(){
+        System.out.println("MC3()");
+    }
+}
