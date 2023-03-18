04fa5e835c5bd63740e90511cc75ef94bf030d54
Elias <elias.miranda@estudante.ufla.br>
2023-03-18 20:54:09 -0300

    ClaseD adcionáda

---
+++ ClasseD.java
@@ -0,0 +1,28 @@
+/*============================================================
+File: ClasseD.java
+Data: 25-11-22_22
+--------------------------------------------------------------
+Graduando: ELIAS DE JESUS MIRANDA 
+ANTONIO MARIA PEREIRA DE RESENDE - DOCENTE - DCC 
+Disciplina:Engenharia de Software 
+TURMA: GCC188-2022/2-V1-10A-G010 
+--------------------------------------------------------------*/
+
+class ClasseD {
+    private int d1;
+    private int d2;
+
+    public ClasseD(){
+        d1 = 0;        
+        d2 = 0;
+    }
+
+    public void mD1(){
+        System.out.print("mD1()");
+    }
+
+    public void mD2(){
+        System.out.print("mD2()");
+    }
+    
+}
